260d680880a009fcf1ba98f12b366f2ce27f9803
刘冠佐 <liuguanzuo@bonc.com.cn>
2019-03-28 15:53:11 +0800

    first-blood

---
+++ src/data/musicDatas.js
@@ -0,0 +1,32 @@
+import gbqq from '../music/gbqq.mp3'
+import kqzdstyhj from '../music/kqzdstyhj.mp3'
+import imageA from '../musicImage/a.jpg'
+import imageB from '../musicImage/b.jpg'
+export const MUSIC_LIST = [
+/*  {
+    id: 1,
+    title: '告白气球',
+    artist: '周杰伦',
+    file: 'https://raw.githubusercontent.com/liuguanzuo/MusicPhoto/master/music/gbqq.mp3',
+    cover: 'https://raw.githubusercontent.com/liuguanzuo/MusicPhoto/master/musicImage/a.jpg'
+  }, {
+    id: 2,
+    title: '空氣中的试听与幻觉',
+    artist: '苏打绿',
+    file: 'https://raw.githubusercontent.com/liuguanzuo/MusicPhoto/master/music/kqzdstyhj.mp3',
+    cover: 'https://raw.githubusercontent.com/liuguanzuo/MusicPhoto/master/musicImage/b.jpg'
+  }*/
+  {
+    id: 1,
+    title: '告白气球',
+    artist: '周杰伦',
+    file: '../music/gbqq.mp3',
+    cover: '../musicImage/a.jpg'
+  },{
+  id: 2,
+  title: '空氣中的试听与幻觉',
+  artist: '苏打绿',
+    file: '../music/kqzdstyhj.mp3',
+    cover: '../musicImage/b.jpg'
+}
+];
